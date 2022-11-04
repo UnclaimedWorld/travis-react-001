349982cadd58bbf29473f8651263616be7021b0e
Damir Khalimov <xalimovdamir@gmail.com>
2022-11-04 10:35:42 +0500

    Test suit to pass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HI
+            learn react
         </p>
       </header>
     </div>
